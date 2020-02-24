1e107c5068e1ff497aa6e4b87ebac69ff0983de2
Patryk Grunt <patryk.grunt@forward3d.com>
2020-02-24 19:54:12 +0100

    use on pull/paths/ and satisfy rubocop.

--- test-1/spec/handler_spec.rb
+++ test-1/spec/handler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Handler do
   specify do
     expect(LOG).to receive(:info).once
-    expect(LOG).to receive(:debug).exactly(2).times
+    expect(LOG).to receive(:debug).twice
 
     described_class.new({}, {})
   end
